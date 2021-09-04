3266b96c319bbcdc18670545d348f0ac09c7d51b
Shibrah Misbah <misbahsh@sheridanc.on.ca>
2021-09-04 13:04:01 -0400

    Second Commit - Setting Up Foundation of Game

--- src/flappy/bird/FlappyBird.java
+++ src/flappy/bird/FlappyBird.java
@@ -5,30 +5,55 @@
  */
 package flappy.bird;
 
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 /**
  *
  * @author cute_
  */
-public class FlappyBird {
+public class FlappyBird implements ActionListener{
     
     public final int WIDTH = 800, HEIGHT = 800;
     
+    //static variable to be used in a static context; main method
     public static FlappyBird flappyBird;
+    
+    public Renderer renderer;
 
     //add a constructor to allow instantiation of class
     public FlappyBird(){
         //create JFrame object using specified height constants
         JFrame jframe = new JFrame();
+        Timer timer = new Timer(20, this);
+        //add renderer object in memory; not null
+        renderer = new Renderer();
         
+        jframe.add(renderer);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setSize(WIDTH, HEIGHT);
+        jframe.setResizable(false);
         jframe.setVisible(true);
+        
+        timer.start();
     }
-  
+    
+
+    public void repaint(Graphics g) {
+        
+    }
+    
     public static void main(String[] args) {
         flappyBird = new FlappyBird();
         
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        renderer.repaint();
     }
     
 }
